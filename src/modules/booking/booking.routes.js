7f977ad42c4fc64dcf627d2c217e23099de869cb
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2026-04-14 20:30:16 +0530

    feat(booking): update booking route to include 'book' in seat booking path

--- src/modules/booking/booking.routes.js
+++ src/modules/booking/booking.routes.js
@@ -5,6 +5,6 @@ import { authenticate } from "../auth/auth.middleware.js";
 const router = Router();
 
 router.get("/seats", controller.getAllSeats);
-router.put("/seats/:seatId", authenticate, controller.bookSeat);
+router.put("/seats/book/:seatId", authenticate, controller.bookSeat);
 
 export default router;
